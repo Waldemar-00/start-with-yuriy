9167ca65f9145f3ea58c162c8d92c18d95e43a3b
uladzimir <01myit@gmail.com>
2023-08-20 17:22:39 +0300

    working with props.children

--- src/components/Costs/CostItem.js
+++ src/components/Costs/CostItem.js
@@ -1,16 +1,17 @@
 import './CostItem.css'
 import CostDate from './CostDate'
+import Card from './Card'
 function CostItem({buy}) {
   const {date, title, sum} = buy
   return (
     <>
-      <div className="cost-item">
+      <Card className={"cost-item"}>
         <CostDate data={date} />
         <div className='cost-item__decription'>
           <h2>{title}</h2>
           <div className='cost-item__sum'>${sum}</div>
         </div>
-      </div>
+      </Card>
     </>
   )
 }
--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -1,13 +1,14 @@
 import CostItem from "./CostItem" 
+import Card from "./Card"
 import './Costs.css'
 function Costs({ purchases }) {
   const [one, two, three] = purchases
   return (
-    <div className='costs'>
+    <Card className={'costs'}>
       <CostItem buy={one} />
       <CostItem buy={two} />
       <CostItem buy={three} />
-    </div>
+    </Card>
   )
 
 }
