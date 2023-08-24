2b4f54c83187cdec0162f38b9057c59415928d0a
uladzimir <01myit@gmail.com>
2023-08-24 20:26:36 +0300

    added interactive Cards

--- src/App.js
+++ src/App.js
@@ -1,36 +1,28 @@
 import Costs from './components/Costs/Costs'
 import NewCost from './components/NewCost/NewCost'
+import {useState} from 'react'
 import { v4 } from 'uuid'
+
+const purchasesData =
+  [
+    { date: new Date(), title: 'Refrigerator', sum: '798', id: v4() },
+    {date: new Date(),title: 'Smartphone',sum: '780',id: v4()},
+    {date: new Date(),title: 'Bookcase',sum: '480',id: v4()}
+  ]
 function App() {
-  const purchasesData =
-    [
-      {
-        date: new Date(),
-        title: 'Refrigerator',
-        sum: '798',
-        id: v4()
-      },
-      {
-        date: new Date(),
-        title: 'Smartphone',
-        sum: '780',
-        id: v4()
-      },
-      {
-        date: new Date(),
-        title: 'Bookcase',
-        sum: '480',
-        id: v4()
-      }
-    ]
+  const [purchases, setPurchases] = useState(purchasesData)
   function getHandlerData(handlerData) {
-    purchasesData.push(handlerData)
-    console.log(purchasesData)
+    handlerData.id = v4()
+    setPurchases((purchases) => {
+      return [
+        handlerData, ...purchases
+      ]
+    })
   }
   return (
     <>
       <NewCost onHandlerData={getHandlerData} />
-      <Costs purchases={purchasesData}/>
+      <Costs purchases={purchases}/>
     </>
   )
 }
