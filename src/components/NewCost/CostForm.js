eced764fbfd3def1f6e916cf149d782b5e688966
uladzimir <01myit@gmail.com>
2023-08-26 16:53:31 +0300

    added interactiving for form button type submit

--- src/components/NewCost/CostForm.js
+++ src/components/NewCost/CostForm.js
@@ -4,6 +4,7 @@ function CostForm({ onHandlerData }) {
   const [title, setTitle] = useState('')
   const [sum, setSum] = useState('')
   const [date, setDate] = useState('')
+  const [unform, setUnform] = useState(false)
   function changeTitleHandler(e) {
     setTitle(e.target.value)
   }
@@ -25,44 +26,51 @@ function CostForm({ onHandlerData }) {
     setDate('')
     onHandlerData(handlerData) 
   }
+  function onButtonHandler() {
+    setUnform(unform => !unform)
+  }
   return (
-    <form onSubmit={submitHandler}>
-      <div className="new-cost__controls">
-        <div className="new-cost__control">
-          <label htmlFor="title">Title</label>
-          <input 
-            type="text" 
-            name='text' 
-            id='title' 
-            value={title} 
-            onChange={changeTitleHandler}
-          />
-        </div>
-        <div className="new-cost__control">
-          <label htmlFor="sum">Sum</label>
-          <input 
-            type="number"
-            min='0.01' step='0.01'
-            name='sum'
-            id='sum'
-            value={sum}
-            onChange={changeSumHandler}
-          />
-        </div>
-        <div className="new-cost__control">
-          <label htmlFor="date">Date</label>
-          <input 
-            type="date" 
-            name='date'
-            min='2022-01-01' max='2026-01-01'
-            value={date}
-            id='date'
-            onChange={changeDateHandler}
-          />
-        </div>
-        <div className="new-cost__actions">
-          <button type='submit'>Added Expense</button>
-        </div>
+    <form onSubmit={submitHandler} className="new-cost__controls">
+      {
+        unform && (
+          <>
+            <div className="new-cost__control">
+              <label htmlFor="title">Title</label>
+              <input
+                type="text"
+                name='text'
+                id='title'
+                value={title}
+                onChange={changeTitleHandler}
+              />
+            </div>
+            <div className="new-cost__control">
+              <label htmlFor="sum">Sum</label>
+              <input
+                type="number"
+                min='0.01' step='0.01'
+                name='sum'
+                id='sum'
+                value={sum}
+                onChange={changeSumHandler}
+              />
+            </div>
+            <div className="new-cost__control">
+              <label htmlFor="date">Date</label>
+              <input
+                type="date"
+                name='date'
+                min='2022-01-01' max='2026-01-01'
+                value={date}
+                id='date'
+                onChange={changeDateHandler}
+              />
+            </div>
+          </>
+        )
+      }
+      <div className="new-cost__actions">
+        <button type='submit' onClick={onButtonHandler}>Added Expense</button>
       </div>
     </form>
   )
